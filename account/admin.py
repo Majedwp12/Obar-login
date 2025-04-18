9aca5765d362858688f5075dfc90d201fdf2e38b
Majed <majedwp12@gmail.com>
2025-04-19 02:31:39 +0330

    Model implementation

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,10 @@
+# users/admin.py
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone', 'last_name', 'email')
+    search_fields = ('phone', 'first_name', 'last_name', 'email')
+    list_filter = ()
+
+admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,39 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError("Phone number is required")
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        """
+        Create and return a superuser with phone number and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(phone, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(unique=True, max_length=15)
+    username = models.CharField(max_length=150, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(blank=True)
+
+    # اضافه کردن فیلدهای is_staff و is_superuser
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
